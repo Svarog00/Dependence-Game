855c354abce31ef50a0c1e47e5bb47fb87c0f4ab
Svarog00 <54817442+Svarog00@users.noreply.github.com>
2021-03-19 20:34:58 +0300

    TileGenerator add

--- GameJamGame/Assets/Scripts/TileGenerator.cs
+++ GameJamGame/Assets/Scripts/TileGenerator.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class TileGenerator : MonoBehaviour
 {
+    public TileGenerator Instance;
     public GameObject[] tilePrefabs;
+    public float shift = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //subscribe on door open event
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnTile()
+    {
+        Instantiate(tilePrefabs[Random.Range(0, tilePrefabs.Length)], transform.forward*shift, transform.rotation);
     }
 }
