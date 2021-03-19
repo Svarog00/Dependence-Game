e223699937ce5c1f19baad8f1ef9d947ee03c826
Svarog00 <54817442+Svarog00@users.noreply.github.com>
2021-03-19 21:26:28 +0300

    Update 2126
    Added camera follow

--- GameJamGame/Assets/Scripts/DeathObject.cs
+++ GameJamGame/Assets/Scripts/DeathObject.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class DeathObject : MonoBehaviour
 {
-
+    public float speed;
+    private bool _canMove = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        //
+        Counter counter = FindObjectOfType<Counter>();
+        counter.OnStartSignal += Counter_OnStartSignal;
+    }
+
+    private void Counter_OnStartSignal(object sender, System.EventArgs e)
+    {
+        _canMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_canMove)
+            transform.position += transform.up * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("BOOO");
+        }
     }
 }
