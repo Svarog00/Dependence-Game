a7deca69e967db35805f96e91cde5cdecded44cc
Svarog00 <54817442+Svarog00@users.noreply.github.com>
2021-03-20 00:50:03 +0300

    Add generation of random items

---
+++ GameJamGame/Assets/Scripts/RoadPointsSpawn.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPointsSpawn : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+
+    public GameObject[] prefabs;
+
+    private int _numOfPoints;
+    private float _weight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _numOfPoints = Random.Range(0, 6);
+        for(int i = 0; i < _numOfPoints; i++)
+        {
+            Instantiate(generateObject(), spawnPoints[i]);
+        }
+    }
+
+    GameObject generateObject()
+    {
+        _weight = Random.Range(0, 100);
+        if (_weight >= 0f && _weight < 35f)
+        {
+            return prefabs[0];
+        }
+        else if (_weight >= 35f && _weight < 71f)
+        {
+            return prefabs[1];
+        }
+        else if (_weight >= 71f && _weight < 83.5f)
+        {
+            return prefabs[2];
+        }
+        else if (_weight >= 83.5f && _weight < 88.5f)
+        {
+            return prefabs[3];
+        }
+        else if (_weight >= 88.5f && _weight < 100f)
+        {
+            return prefabs[4];
+        }
+        else return null;
+    }
+}
