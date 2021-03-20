306deb30b631d31cc6d14bed9de082688f812204
grravve <55639256+grravve@users.noreply.github.com>
2021-03-20 11:24:42 +0300

    Event Button+Door added

--- GameJamGame/Assets/Scripts/DoorButton.cs
+++ GameJamGame/Assets/Scripts/DoorButton.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class DoorButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public event EventHandler OnButtonPressed;
+
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            OnButtonPressed?.Invoke(this, EventArgs.Empty);
+            animator.Play("DoorButton_Pressed");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if(collision.tag == "Player")
+        {
+            animator.Play("DoorButton_Idle");
+        }
     }
 }
