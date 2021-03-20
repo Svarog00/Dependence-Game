9e2b461855af6378950bdeb26f33a216a4759c15
grravve <55639256+grravve@users.noreply.github.com>
2021-03-20 18:02:50 +0300

    Menu edited

---
+++ GameJamGame/Assets/Scripts/UI/RetryButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RetryButton : MonoBehaviour
+{
+    public GameObject background;
+    public Sprite newBackground;
+    private Image _oldImage;
+    private Image _currentImage;
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+        _oldImage = background.GetComponent<Image>();
+        _currentImage = _oldImage;
+    }
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("SOSI");
+        _currentImage.sprite = newBackground;
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("SOSI SNOVA");
+        _currentImage.sprite = _oldImage.sprite;
+    }
+
+}
